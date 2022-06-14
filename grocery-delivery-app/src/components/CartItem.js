6aa9ba85dd49f704e06c16577a09cab228b21f59
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-06-14 11:50:21 +0530

    dispatch item

--- grocery-delivery-app/src/components/CartItem.js
+++ grocery-delivery-app/src/components/CartItem.js
@@ -11,7 +11,7 @@ let CartItem = () =>{
          dispatch(calcTotal())
          dispatch(getTax())
          dispatch(getGrandTotal())
-     },[])
+     })
     return(
         <React.Fragment>
              <div className="container">
